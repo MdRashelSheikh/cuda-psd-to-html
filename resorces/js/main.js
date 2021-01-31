bc382a05714e8518747fdeb7cbb562dd4dd3e00a
mdrashelsheikh <raselk040@gmail.com>
2021-01-31 19:49:54 +0600

    cuda psd to html

---
+++ resorces/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    //sticky bar
+    $(".js--services-section").waypoint(function(direction){
+        if(direction=="down"){
+            $("nav").addClass("sticky");
+    } else{
+        $("nav").removeClass("sticky");
+    }
+
+    });
+    
+    
+    
+    
+    //mixitup portfolio section
+ var mixer =mixitup('.container');
+});
